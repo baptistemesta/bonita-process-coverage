3557b262ff84d8c3bef31a968c2c95fc5b1abac7
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-12-30 15:07:23 +0100

    WIP

--- src/test/java/org/bonitasoft/engine/ProcessCoverageIT.java
+++ src/test/java/org/bonitasoft/engine/ProcessCoverageIT.java
@@ -2,12 +2,18 @@ package org.bonitasoft.engine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.bonitasoft.engine.test.PlatformTestUtil;
 import org.junit.Test;
 
-public class ProcessCoverageTest {
+public class ProcessCoverageIT {
 
     @Test
-    public void generateCoverageForProcess(){
+    public void generateCoverageForProcess() throws Exception {
+
+        PlatformTestUtil platformTestUtil = new PlatformTestUtil();
+
+        platformTestUtil.createInitializeAndStartPlatformWithDefaultTenant(false);
+
         //given
         ProcessCoverage processCoverage = new ProcessCoverage();
 
